5d8d9fc8e87558ac7283b5870aecf09b87191a65
Kristoffer Cobley <kcobley@gmail.com>
2011-10-04 13:54:49 +0200

    succ defined, intToNat defined, broken natToInt.

--- Prolog/assignment.pl
+++ Prolog/assignment.pl
@@ -1,1 +1,21 @@
-hejj
+% natural numbers, represented using the successor definition.
+
+succ(0).
+succ(s(N)) :- succ(N).
+    
+
+% returns an int.
+% does not work in current form.
+natToInt(N,I) :-
+    N =:= intToNat(I),
+    I1 is I+1,
+    natToInt(N,I1).
+
+
+% returns a natural number.
+
+intToNat(0,0).
+intToNat(I,s(N)) :- 
+    I > 0,
+    I1 is I-1,
+    intToNat(I1,N).
