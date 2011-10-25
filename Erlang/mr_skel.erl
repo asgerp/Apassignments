72dd910f93c126300be94820be4222676d571b17
Asger Pedersen <asger.pedersen@gmail.com>
2011-10-25 10:46:32 +0200

    clean up and tests
    Clean up of mr_skel and moving all the test over in the mister
    mr_test.erl
    Much more success

--- Erlang/mr_skel.erl
+++ Erlang/mr_skel.erl
@@ -22,7 +22,7 @@ stop(Pid) ->
 %%% Define a MapReduce job
 job(CPid, MapFun, RedFun, RedInit, Data) -> 
     rpc(CPid, {init, MapFun, RedFun, RedInit, Data}),
-    rpc(CPid, {start,Data}).
+    rpc(CPid, {start, Data}).
     
 
 %%%% Internal implementation
@@ -30,9 +30,9 @@ job(CPid, MapFun, RedFun, RedInit, Data) ->
 %%% Make N mappers and one reducer
 init(N) ->  
     init(N,spawn(fun() -> reducer_loop() end),[]).
-init(N,Red,Map) when N > 0 ->
-    init(N-1,Red,[spawn(fun() -> mapper_loop(Red,fun() -> im_a_mapper end) end)]++Map); 
-init(0,Red,Map) -> {Red,Map}.
+init(N, Red, Map) when N > 0 ->
+    init(N-1, Red, [spawn(fun() -> mapper_loop(Red, fun() -> im_a_mapper end) end)]++Map); 
+init(0, Red, Map) -> {Red, Map}.
 
 
 %% synchronous communication
@@ -78,21 +78,21 @@ coordinator_loop(Reducer, Mappers) ->
 	{From, {init, MapFun, RedFun, RedInit, Data}} ->
 	    Len = length(Data),
 	    info(Reducer,{self(),{start,RedFun, RedInit, Len, From}}),
-	    loop(Mappers, MapFun),
+	   init_mappers(Mappers, MapFun),
 	    reply_ok(From),
 	    coordinator_loop(Reducer, Mappers);
-	{From, {start, Data}} ->
+	{_, {start, Data}} ->
 	    send_data(Mappers, Data),
 	    coordinator_loop(Reducer, Mappers);
-	{From, {done, Result, Jid}} ->
+	{_, {done, Result, Jid}} ->
 	    reply_ok(Jid, Result),
 	    coordinator_loop(Reducer, Mappers)
     end.
 
-loop([Mid|Mappers], MapFun)->
+init_mappers([Mid|Mappers], MapFun)->
     info(Mid, {self(), {init, MapFun}}),
-    loop(Mappers, MapFun);
-loop([],_) ->
+   init_mappers(Mappers, MapFun);
+init_mappers([],_) ->
     done.
 
 
@@ -126,7 +126,7 @@ gather_data_from_mappers(Fun, Acc, Missing) ->
     receive
 	{stop_gather, Acc} ->
 	    Acc;
-	{From, {result, ChunkOfData}} ->
+	{_, {result, ChunkOfData}} ->
 %	    io:format("chunk ~p ~n",[ChunkOfData]),
 	    Res = (lists:foldl(Fun, Acc, ChunkOfData)),
 	    Miss = Missing - 1,
@@ -155,145 +155,3 @@ mapper_loop(Reducer, Fun) ->
 	    mapper_loop(Reducer, Fun)
     end.
 
-test_sum() ->
-    {ok, MR}  = mr_skel:start(3),
-    {ok, Sum} = mr_skel:job(MR,
-                       fun(X) -> X end,
-                       fun(X,Acc) -> X+Acc end,
-                       0,
-                       []++lists:seq(1,10)),
-    {ok, Fac} = mr_skel:job(MR,
-                       fun(X) -> X end,
-		       fun(X,Acc) -> X*Acc end,
-		       1,
-		       lists:seq(1,10)),
-    mr_skel:stop(MR),
-    {Sum, Fac}.
-
-
-    
-
-test_job() ->
-    {ok, MR} = mr_skel:start(3),
-    {ok, Sum} = mr_skel:job(MR,
-			    fun(X) -> X end,
-			    fun(X,Acc) -> X+Acc end,
-			    0,
-			    lists:zipwith(fun(X, Y) -> [X*Y/Y] end, lists:seq(1,10), lists:seq(1,10))),
-    mr_skel:stop(MR),
-    Sum.
-	
-test_fac() ->
-    {ok, MR} = mr_skel:start(3),
-    {ok, Fac} = mr_skel:job(MR,
-			    fun(X) -> X end,
-			    fun(X,Acc) -> X*Acc end,
-			    1,
-			    lists:seq(1,10)),
-    mr_skel:stop(MR),
-    Fac.
-
-
-list_of_tracks([],CleanTracks)->
-    CleanTracks;
-list_of_tracks([H | Tail],CleanTracks) ->
-    Track = read_mxm:parse_track(H),
-    Words = element(3,Track),
-    list_of_tracks(Tail, [Words|CleanTracks]).
-    
-
-word_data() ->
-    Now = erlang:now(),
-    WordData = read_mxm:from_file("mxm_dataset_test.txt"),
-    Tracks = element(2,WordData),
-    Res = list_of_tracks(Tracks,[]),
-    Then = erlang:now(),
-    Time = timer:now_diff(Then, Now),
-    io:format(" time: ~p~n",[Time/1000000]),
-    Res.
-
-%%% 44846399
-total_words() ->
-    Now = erlang:now(),
-    {ok, MR}        = mr_skel:start(25),
-    {ok, NoOfWords} = mr_skel:job(MR,
-				  fun(H) -> element(2,H) end,
-				  fun(X,Acc)   -> X+Acc end,
-				  0,
-				  word_data()),
-    mr_skel:stop(MR),
-    Done = erlang:now(),
-    Time = timer:now_diff(Done, Now),
-    io:format(" time: ~p~n",[Time/1000000]),
-    NoOfWords.
-			
-%%% Preprocessing for averages	
-%%% We need the data to be in the form [[[{integer(),integer()}]], [[...]]]  
-list_of_tracks2([],CleanTracks)->
-    CleanTracks;
-list_of_tracks2([H | Tail],CleanTracks) ->
-    Track = read_mxm:parse_track(H),
-    Words = element(3,Track),
-    list_of_tracks2(Tail, [[Words]|CleanTracks]).
-    
-word_data2() ->
-    Now = erlang:now(),
-    WordData = read_mxm:from_file("mxm_dataset_test.txt"),
-    Tracks = element(2,WordData),
-    Res = list_of_tracks2(Tracks,[]),
-    Then = erlang:now(),
-    Time = timer:now_diff(Then, Now),
-    io:format("Preprocessing time: ~p~n",[Time/1000000]),
-    Res.					  
-
-
-%%% To calculate the average number of different words, the mappers calculate the length of the
-%%% list containing the word count. The reducer store the lengths in a tuple {Words, Songs}
-%%% result 80.0210099800968 for largest dataset
-avg() ->
-    Data = word_data2(),
-    Now = erlang:now(),
-    {ok, MR}        = mr_skel:start(1),
-    {ok, AvgDiffWords} = mr_skel:job(MR,
-				     fun(X)  -> length(X) end,
-				     fun(X, Acc) -> {Words, Songs} = Acc,
-						    {Words + X, Songs +1}
-				     end,
-				     {0,0},
-				     Data),
-    {ok, AvgWords} = mr_skel:job(MR,
-				 fun(X)  -> lists:foldl(fun(Y, Acc) -> element(2,Y)+Acc end, 0 ,X) end,
-				 fun(X, Acc) -> {Words, Songs} = Acc,
-						{Words + X, Songs +1}
-				 end,
-				 {0,0},
-				 Data),
-    mr_skel:stop(MR),
-    {W, S} = AvgDiffWords,
-    {Ws, Ss} = AvgWords,
-    Done = erlang:now(),
-    Time = timer:now_diff(Done, Now),
-    io:format("MapReduce time: ~p~n",[Time/1000000]),
-    {W/S, Ws/Ss}.
-
-%% vi skal have [[trackid],[words],[trackid],[words]]
-%% mapper = if [trackid] -> [trackid] else [words] -> [1|0]
-%% if word is contained in song 1 else 0
-%% reducer = if 1 then songid in list else trow it away
-
-grep(Word)->
-    Data = word_data2(),
-    Now = erlang:now(),
-    {ok, MR}        = mr_skel:start(1),
-    {ok, NameOfSongs} = mr_skel:job(MR,
-				     fun(X)  -> length(X) end,
-				     fun(X, Acc) -> {Words, Songs} = Acc,
-						    {Words + X, Songs +1}
-				     end,
-				     {0,0},
-				     Data),
-    mr_skel:stop(MR),
-    Done = erlang:now(),
-    Time = timer:now_diff(Done, Now),
-    io:format("MapReduce time: ~p~n",[Time/1000000]),
-    NameOfSongs.
---
+++ Erlang/mr_test.erl
@@ -0,0 +1,149 @@
+%%% The mister test for the mister map reduce
+
+-module(mr_test).
+
+-export([test_sum/0, test_job/0, test_fac/0,total_words/0, list_of_tracks/2, word_data/0, avg/0]).
+
+
+test_sum() ->
+    {ok, MR}  = mr_skel:start(3),
+    {ok, Sum} = mr_skel:job(MR,
+			    fun(X) -> X end,
+			    fun(X,Acc) -> X+Acc end,
+			    0,
+			    lists:zipwith(fun(X, Y) -> [X*Y/Y] end, lists:seq(1, 10), lists:seq(1, 10))),
+    {ok, Fac} = mr_skel:job(MR,
+			    fun(X) -> X end,
+			    fun(X,Acc) -> X*Acc end,
+			    1,
+			    lists:zipwith(fun(X, Y) -> [X*Y/Y] end, lists:seq(1, 10), lists:seq(1, 10))),
+    mr_skel:stop(MR),
+    {round(Sum), round(Fac)}.
+
+
+    
+
+test_job() ->
+    {ok, MR} = mr_skel:start(3),
+    {ok, Sum} = mr_skel:job(MR,
+			    fun(X) -> X end,
+			    fun(X,Acc) -> X+Acc end,
+			    0,
+			    lists:zipwith(fun(X, Y) -> [X*Y/Y] end, lists:seq(1, 10), lists:seq(1, 10))),
+    mr_skel:stop(MR),
+    Sum.
+	
+test_fac() ->
+    {ok, MR} = mr_skel:start(3),
+    {ok, Fac} = mr_skel:job(MR,
+			    fun(X) -> X end,
+			    fun(X,Acc) -> X*Acc end,
+			    1,
+			    lists:seq(1,10)),
+    mr_skel:stop(MR),
+    Fac.
+
+
+list_of_tracks([],CleanTracks)->
+    CleanTracks;
+list_of_tracks([H | Tail],CleanTracks) ->
+    Track = read_mxm:parse_track(H),
+    Words = element(3,Track),
+    list_of_tracks(Tail, [Words|CleanTracks]).
+    
+
+word_data() ->
+    Now = erlang:now(),
+    WordData = read_mxm:from_file("mxm_dataset_test.txt"),
+    Tracks = element(2,WordData),
+    Res = list_of_tracks(Tracks,[]),
+    Then = erlang:now(),
+    Time = timer:now_diff(Then, Now),
+    io:format("Preprocessing time: ~p~n",[Time/1000000]),
+    Res.
+
+%%% 44846399
+total_words() ->
+    Now = erlang:now(),
+    {ok, MR}        = mr_skel:start(25),
+    {ok, NoOfWords} = mr_skel:job(MR,
+				  fun(H) -> element(2,H) end,
+				  fun(X,Acc)   -> X+Acc end,
+				  0,
+				  word_data()),
+    mr_skel:stop(MR),
+    Done = erlang:now(),
+    Time = timer:now_diff(Done, Now),
+    io:format("MapReduce time: ~p~n",[Time/1000000]),
+    NoOfWords.
+			
+%%% Preprocessing for averages	
+%%% We need the data to be in the form [[[{integer(),integer()}]], [[...]]]  
+list_of_tracks2([],CleanTracks)->
+    CleanTracks;
+list_of_tracks2([H | Tail],CleanTracks) ->
+    Track = read_mxm:parse_track(H),
+    Words = element(3,Track),
+    list_of_tracks2(Tail, [[Words]|CleanTracks]).
+    
+word_data2() ->
+    Now = erlang:now(),
+    WordData = read_mxm:from_file("mxm_dataset_test.txt"),
+    Tracks = element(2,WordData),
+    Res = list_of_tracks2(Tracks,[]),
+    Then = erlang:now(),
+    Time = timer:now_diff(Then, Now),
+    io:format("Preprocessing time: ~p~n",[Time/1000000]),
+    Res.					  
+
+
+%%% To calculate the average number of different words, the mappers calculate the length of the
+%%% list containing the word count. The reducer store the lengths in a tuple {Words, Songs}
+%%% result 80.0210099800968 for largest dataset
+avg() ->
+    Data = word_data2(),
+    Now = erlang:now(),
+    {ok, MR}        = mr_skel:start(1),
+    {ok, AvgDiffWords} = mr_skel:job(MR,
+				     fun(X)  -> length(X) end,
+				     fun(X, Acc) -> {Words, Songs} = Acc,
+						    {Words + X, Songs +1}
+				     end,
+				     {0,0},
+				     Data),
+    {ok, AvgWords} = mr_skel:job(MR,
+				 fun(X)  -> lists:foldl(fun(Y, Acc) -> element(2,Y)+Acc end, 0 ,X) end,
+				 fun(X, Acc) -> {Words, Songs} = Acc,
+						{Words + X, Songs +1}
+				 end,
+				 {0,0},
+				 Data),
+    mr_skel:stop(MR),
+    {W, S} = AvgDiffWords,
+    {Ws, Ss} = AvgWords,
+    Done = erlang:now(),
+    Time = timer:now_diff(Done, Now),
+    io:format("MapReduce time: ~p~n",[Time/1000000]),
+    {W/S, Ws/Ss}.
+
+%% vi skal have [[trackid],[words],[trackid],[words]]
+%% mapper = if [trackid] -> [trackid] else [words] -> [1|0]
+%% if word is contained in song 1 else 0
+%% reducer = if 1 then songid in list else trow it away
+
+grep(Word)->
+    Data = word_data2(),
+    Now = erlang:now(),
+    {ok, MR}        = mr_skel:start(1),
+    {ok, NameOfSongs} = mr_skel:job(MR,
+				     fun(X)  -> length(X) end,
+				     fun(X, Acc) -> {Words, Songs} = Acc,
+						    {Words + X, Songs +1}
+				     end,
+				     {0,0},
+				     Data),
+    mr_skel:stop(MR),
+    Done = erlang:now(),
+    Time = timer:now_diff(Done, Now),
+    io:format("MapReduce time: ~p~n",[Time/1000000]),
+    NameOfSongs.
